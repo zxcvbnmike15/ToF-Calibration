999bc3850289285721b22e0ac0f5a931ac3cb523
Mike Smith <msmith@ccom.unh.edu>
2020-06-26 12:43:53 -0400

    Get Samples update
    Got rid of globals. I think there is a weird thing going on with the masking, but it ain't my job to change it

--- process_depth_regions.m
+++ process_depth_regions.m
@@ -5,14 +5,24 @@ function process_depth_regions(cal_info)
 % SYNTAX:
 %
 
+%% Subset the data
+depth_files = cal_info.files.depth;
+options = cal_info.options;
+
+depth_plane_mask = cal_info.depth.depth_plane_mask;
+depth_plane_poly = cal_info.depth.depth_plane_poly;
+depth_plane_points = cal_info.depth.depth_plane_points;
+depth_plane_disparity = cal_info.depth.depth_plane_disparity;
+
 %% Get depth plane masks
-if isempty(cal_info.depth.depth_plane_mask)
-    cal_info.depth = select_planes(cal_info.files.depth, cal_info.depth);
+if isempty(depth_plane_mask)
+    [depth_plane_poly, depth_plane_mask] = select_planes(depth_files, ...
+        depth_plane_poly, depth_plane_mask);
 end
 
 %% Process depth samples
 fprintf('Extracting disparity samples...\n');
-[depth_plane_points,depth_plane_disparity] = get_depth_samples(cal_info.files.depth,cal_info.depth,cal_info.options);
+[depth_plane_points,depth_plane_disparity] = get_depth_samples(depth_files,depth_plane_mask,options);
 initial_count = sum(cellfun(@(x) size(x,2),depth_plane_points));
 
 [depth_plane_points,depth_plane_disparity] = remove_invalid(depth_plane_points,depth_plane_disparity,options);
@@ -21,3 +31,10 @@ clean_count = sum(cellfun(@(x) size(x,2),depth_plane_points));
 [depth_plane_points,depth_plane_disparity] = reduce_depth_samples(depth_plane_points,depth_plane_disparity,max_depth_sample_count);
 total_count = sum(cellfun(@(x) size(x,2),depth_plane_points));
 fprintf('Initial disparity samples: %d\nClean disparity samples: %d\nUsing %d.\n',initial_count,clean_count,total_count);
+
+
+%% Place results in cal_info
+ cal_info.depth.depth_plane_mask = depth_plane_mask;
+ cal_info.depth.depth_plane_poly = depth_plane_poly;
+ cal_info.depth.depth_plane_points = depth_plane_points;
+ cal_info.depth.depth_plane_disparity = depth_plane_disparity;
