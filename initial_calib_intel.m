1aba19079e2355f64f2c39431e729d1fbc5a10ee
Mike Smith <msmith@ccom.unh.edu>
2020-06-29 13:19:43 -0400

    Replacement initial_calib_file

---
+++ initial_calib_intel.m
@@ -0,0 +1,13 @@
+function initial_calib_intel(options)
+
+%% initialize calibration
+global calib0 rfiles
+
+%do corner-based calibration
+do_initial_conf_calib(options.use_fixed_ini);
+
+if(options.color_present)
+    do_initial_rgb_calib();
+    %relative transformation initialization
+    [calib0.cR,calib0.ct]=estimate_relative(calib0);
+end
