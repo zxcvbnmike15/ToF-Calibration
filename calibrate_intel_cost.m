d2e0a9456f03f2e142aaccf0b2d7e4f0728897fe
Mike Smith <msmith@ccom.unh.edu>
2020-07-06 17:15:58 -0400

    Woops I made a mistake there
    Should have kept it the way it was

--- calibrate_intel_cost.m
+++ calibrate_intel_cost.m
@@ -1,26 +1,17 @@
-function [cost, comp ] =calibrate_intel_cost(cal_info)
+function [cost, comp ] =calibrate_intel_cost(calib,depth_points,depth_depth,conf_grid_x,conf_grid_p,options,rgb_grid_p)
 % cost=calibrate_intel_cost_depth(params,options,depth_plane_points,depth_plane_disparity)
 % Cost for depth images, used by calibrate_intel_cost;
 % Uses both depth and corner-based cost on confidence map
 %
 % Kinect calibration toolbox by DHC
-%% Unpack
-calib = cal_info.calib0;
-depth_points = cal_info.depth.depth_plane_points;
-depth_disparity = cal_info.depth.depth_plane_disparity;
-conf_grid_x = cal_info.confidence.conf_grid_x;
-conf_grid_p = cal_info.confidence.conf_grid_p;
-options = cal_info.options;
-rgb_grid_p = cal_info.color.rgb_grid_p;
 
-%% Calculate depth cost
-[cost_depth, comp] = calibrate_intel_cost_depth(calib,depth_points,depth_disparity,conf_grid_x,conf_grid_p,options);
+[cost_depth, comp] = calibrate_intel_cost_depth(calib,depth_points,depth_depth,conf_grid_x,conf_grid_p,options);
 
 if(sum(isnan(cost_depth))>0)
     warning('NaN values in cost');
 end
 
-if options.color_present && exist('rgb_grid_p','var')
+if (exist('rgb_grid_p','var'))
     cost_rgb = calibrate_intel_cost_color(calib, conf_grid_x, rgb_grid_p);
     
     comp = [comp; repmat('R',length(cost_rgb),1)];
